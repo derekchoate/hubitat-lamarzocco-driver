c647c6d8c147d4bac0ad4d2ef1f15e8ba9d2ce17
Derek Choate <derek.choate@gmail.com>
2024-01-13 11:24:47 +0000

    fixed copyright notice

--- lm-espresso-machine-library.groovy
+++ lm-espresso-machine-library.groovy
@@ -1,7 +1,7 @@
 /**
  *  La Marzocco Espresso Machine driver for Hubitat
  *
- *  Copyright 2016 Stuart Buchanan
+ *  Copyright 2024 Derek Choate
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
